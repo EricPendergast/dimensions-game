086cd7e15bd8feb1dc02ffdb5e38970d88263dd8
Eric Pendergast <epender@umich.edu>
2017-06-01 16:23:47 -0400

    Added level saving and loading.

--- src/environment.py
+++ src/environment.py
@@ -1,20 +1,25 @@
 import settings
+import json
+
 class Level:
     def __init__(self):
+        # grid contains all the blocks in the current level. If a coordinate is
+        # not one of the keys in grid, it is assumed to be air
         self.grid = {}
-        self.grid[(5,5)] = "a"
-        self.grid[(6,5)] = "a"
-        self.grid[(7,5)] = "a"
-        self.grid[(8,5)] = "a"
-        self.grid[(9,5)] = "a"
+        self.load("1.lvl")
+        # self.save("1.lvl")
     
     def render(self, renderer):
-        print("Rendering")
-        
-        # renderer.drawRect(60,60,100,5000)
-        
         for square in self.grid.keys():
-            point = [i*settings.GRID_SIZE for i in square]
+            # point = [i*settings.GRID_SIZE for i in square]
+            point = [int(i)*settings.GRID_SIZE for i in square.split(",")]
             renderer.drawRect(*(point + [settings.GRID_SIZE]*2))
         
+    def save(self, filename):
+        file = open(settings.LEVEL_SAVES_PATH + filename, "w")
+        file.write(json.dumps(self.grid))
+        
+    def load(self, filename):
+        file = open(settings.LEVEL_SAVES_PATH + filename, "r")
+        self.grid = json.loads(file.read())
         
